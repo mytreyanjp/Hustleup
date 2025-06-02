ad68934e20ffdb71fefa69fbe77d05ae461c19dd
Mytreyan JP <mytreyan197@gmail.com>
2025-06-02 09:51:47 +0000

    the hamburger menu's width is so big when viewport is in the size of a mobile

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -308,7 +308,7 @@ export default function Navbar() {
                       </Avatar>
                     </Button>
                   </DropdownMenuTrigger>
-                  <DropdownMenuContent className="w-56" align="end" forceMount>
+                  <DropdownMenuContent className="w-52 sm:w-56" align="end" forceMount>
                     <DropdownMenuLabel className="font-normal">
                       <div className="flex flex-col space-y-1">
                         <p className="text-sm font-medium leading-none truncate">
